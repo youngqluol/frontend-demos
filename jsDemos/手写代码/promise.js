7d89d4cc76d2d19db5a87700cd24d7f4bc8359de
younqluol <953342912@qq.com>
2021-02-26 15:02:52 +0800

    手写代码

--- jsDemos/手写代码/promise.js
+++ jsDemos/手写代码/promise.js
@@ -1,6 +1,33 @@
 class MyPromise {
   constructor(fn) {
-    this.fn = fn;
-    fn();
-  }
+    this.PENDING = 'pending';
+    this.RESOLVED = 'resolved';
+    this.REJECTED = 'rejeced';
+    this.status = this.PENDING;
+    try{
+      fn(resolve, reject);
+    } catch(e) {
+      reject(e);
+    }
+  };
+  resolve(value) {
+      if(this.status === this.PENDING) {
+        
+      } 
+  };
 }
+
+// 先写一个实例参考
+const promise = new Promise((resolve, reject) => {
+  if (Math.random() * 10 > 5) resolve(1)
+  else reject('error');
+})
+
+promise.then(
+  () => {
+
+  },
+  () => {
+
+  }
+)
