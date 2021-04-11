d8bf9d2ce114280a10e5650e4bba58104d746109
刘巧萍 <Emily@liuqiaopingdeMacBook-Air.local>
2021-04-11 13:03:37 +0800

    react

--- jsDemos/手写代码/suanfa.js
+++ jsDemos/手写代码/suanfa.js
@@ -1,3 +1,4 @@
+// 数组前i个之和 组成新数组
 function fn(arr) {
     if(arr.length === 0) return [];
     let _arr = []
@@ -7,4 +8,19 @@ function fn(arr) {
         _arr.push(sumItem);
     })
     return _arr;
+}
+
+// 最长的子字符串不重复字符 的长度
+function getlongest(str) {
+    let arr = [];
+    let Arrlength = 0;
+    for (let i = 1; i < str.length; i++) {
+        const index = arr.indexOf(str[i]);
+        if(index !== -1) {
+            arr.splice(0, index+1);
+        } 
+        arr.push(str[i]);
+        Arrlength = Math.max(arr.length, Arrlength);
+    };
+    return Arrlength;
 }
