303ab447ed287d2b399dea00961c5adf39e639e6
yongqing_luo <yongqing_luo@kingdee.com>
2021-07-16 18:19:01 +0800

    项目配置打包后生成zip包

--- vueProjects/drag-demo/vue.config.js
+++ vueProjects/drag-demo/vue.config.js
@@ -1,4 +1,23 @@
+const FileManagerPlugin = require('filemanager-webpack-plugin');
+const projectName = require('./package.json').name;
+const fileManagerPlugin = new FileManagerPlugin({
+  events: {
+    onEnd: {
+      mkdir: ['./dist'],
+      delete: [
+        `./dist/dist.zip`
+      ],
+      archive: [
+        {source: `./dist`, destination: `./dist/dist.zip`}
+      ]
+    }
+  }
+});
+
 module.exports = {
+  configureWebpack: {
+    plugins: [fileManagerPlugin]
+  },
   chainWebpack: config => {
     config.module
       .rule('eslint')
@@ -10,3 +29,12 @@ module.exports = {
       });
   }
 };
+
+function formatDate() {
+  const year = new Date().getFullYear();
+  const month = new Date().getMonth() + 1 > 10 ? new Date().getMonth() : '0' + (new Date().getMonth() + 1);
+  const day = new Date().getDate() > 10 ? new Date().getDate() : '0' + new Date().getDate();
+  const hour = new Date().getHours() > 10 ? new Date().getHours() : '0' + new Date().getHours();
+  const minute = new Date().getMinutes() > 10 ? new Date().getMinutes() : '0' + new Date().getMinutes();
+  return year + month + day + hour + minute;
+}
