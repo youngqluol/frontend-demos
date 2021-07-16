2d500fe5a02bfbe086e58ab9401733338476fe64
luoyongqing <Emily@liuqiaopingdeMacBook-Air.local>
2021-07-16 23:01:52 +0800

    项目配置命令行打包及analyzer

--- vueProjects/drag-demo/src/components/HelloWorld.vue
+++ vueProjects/drag-demo/src/components/HelloWorld.vue
@@ -1,25 +1,39 @@
 <template>
-  <el-form ref="dynamicValidateForm"
-           :model="dynamicValidateForm"
-           label-width="100px"
-           class="demo-dynamic">
-    <el-form-item
-      v-for="(domain, index) in dynamicValidateForm.domains"
-      :key="domain.key"
-      :label="'域名' + index"
-      :prop="'domains.' + index + '.value'"
-      :rules="domain.rules"
-    >
-      <el-input v-model="domain.value"></el-input><el-button @click.prevent="removeDomain(domain)">删除</el-button>
-    </el-form-item>
-    <el-form-item>
-      <el-button type="primary"
-                 @click="submitForm('dynamicValidateForm')">提交</el-button>
-      <el-button @click="addDomain">新增域名</el-button>
-      <el-button @click="resetForm('dynamicValidateForm')">重置</el-button>
-      <el-button @click="switchData()">改变数据</el-button>
-    </el-form-item>
-  </el-form>
+  <div class="demo-warp">
+    <el-form ref="dynamicValidateForm"
+             :model="dynamicValidateForm"
+             label-width="100px"
+             label-position="top"
+             class="demo-dynamic">
+      <el-form-item
+        v-for="(domain, index) in dynamicValidateForm.domains"
+        :key="domain.key"
+        :label="'域名' + index"
+        :prop="'domains.' + index + '.value'"
+        :rules="domain.rules"
+      >
+        <el-select v-model="value"
+                   class="el-select"
+                   placeholder="请选择">
+          <el-option
+            v-for="item in options"
+            :key="item.value"
+            :label="item.label"
+            :value="item.value">
+          </el-option>
+        </el-select>
+        <el-input v-model="domain.value"
+                  class="el-input"></el-input><el-button @click.prevent="removeDomain(domain)">删除</el-button>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary"
+                   @click="submitForm('dynamicValidateForm')">提交</el-button>
+        <el-button @click="addDomain">新增域名</el-button>
+        <el-button @click="resetForm('dynamicValidateForm')">重置</el-button>
+        <el-button @click="switchData()">改变数据</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
 </template>
 <script>
 export default {
@@ -33,7 +47,24 @@ export default {
             required: true, message: '域名不能为空', trigger: 'blur'
           }
         }]
-      }
+      },
+      options: [{
+        value: '选项1',
+        label: '黄金糕'
+      }, {
+        value: '选项2',
+        label: '双皮奶'
+      }, {
+        value: '选项3',
+        label: '蚵仔煎'
+      }, {
+        value: '选项4',
+        label: '龙须面'
+      }, {
+        value: '选项5',
+        label: '北京烤鸭'
+      }],
+      value: ''
     };
   },
   methods: {
@@ -103,19 +134,16 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-  h3 {
-    margin: 40px 0 0;
+<style scoped> 
+  .demo-warp {
+    width: 500px;
   }
-  ul {
-    list-style-type: none;
-    padding: 0;
-  }
-  li {
-    display: inline-block;
-    margin: 0 10px;
-  }
-  a {
-    color: #42b983;
+
+  .el-input {
+      width: 20%;
+    }
+
+  .el-select {
+      width: 20%;
   }
 </style>
