a2d425b3cf35b7be7fafd63932bb5c31a8712537
yongqing_luo <yongqing_luo@kingdee.com>
2021-07-30 18:30:54 +0800

    配置mongodb云端

--- vueProjects/drag-demo/server/db/index.js
+++ vueProjects/drag-demo/server/db/index.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
-const MONDB_URL = "mongodb://127.0.0.1:27017/user"; // 本地mongodb数据库地址
+// const MONDB_URL = "mongodb://127.0.0.1:27017/user"; // 本地mongodb数据库地址
+const MONDB_URL = "mongodb+srv://youngqluol:lyq7285600@cluster0.49eox.mongodb.net/user?retryWrites=true&w=majority"; // 云端mongodb数据库地址
 
 mongoose.connect(MONDB_URL, {
   useNewUrlParser: true,
