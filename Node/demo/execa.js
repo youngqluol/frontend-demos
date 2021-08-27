ac839a7e7ab8dae626602ed1da9ba3d1c6f08b65
yongqing_luo <yongqing_luo@kingdee.com>
2021-08-27 19:50:01 +0800

    node

--- Node/demo/execa.js
+++ Node/demo/execa.js
@@ -1,9 +1,14 @@
 const execa = require('execa');
+const path = require('path');
+const child_process = require('child_process');
+const util = require('util');
+const exec = util.promisify(child_process.exec);
+const resolve = p => path.resolve(__dirname, p);
 
 (async () => {
-  const { stdout } = await execa.command('cd ../../');
-  await execa('git', ['status']);
-  await execa('git', ['add', '.']);
-  await execa('git', ['commit', '-m', 'update node']);
-  console.log(typeof stdout, 1);
+  const { stdout } = await exec('cd ../../', {cwd: resolve('./demo')});
+  // await execa('git', ['status']);
+  // await execa('git', ['add', '.']);
+  // await execa('git', ['commit', '-m', 'update node']);
+  console.log(stdout);
 })();
