b0f69a85a1e804646b02019349968aeea003a5b8
luoyongqing <Emily@liuqiaopingdeMacBook-Air.local>
2021-08-28 00:40:11 +0800

    up

--- Node/demo/execa.js
+++ Node/demo/execa.js
@@ -1,14 +1,21 @@
-const execa = require('execa');
+// const execa = require('execa');
 const path = require('path');
+const process = require('process');
 const child_process = require('child_process');
 const util = require('util');
 const exec = util.promisify(child_process.exec);
 const resolve = p => path.resolve(__dirname, p);
 
-(async () => {
-  const { stdout } = await exec('cd ../../', {cwd: resolve('./demo')});
+async function fn() {
+  const { stdout } = await exec('ls', {cwd: resolve('../demo')});
+  console.log(stdout);
   // await execa('git', ['status']);
   // await execa('git', ['add', '.']);
   // await execa('git', ['commit', '-m', 'update node']);
-  console.log(stdout);
-})();
+  // console.log(stdout);
+  // console.log(`初始目录：${process.cwd()}`)
+  // process.chdir(resolve('../../Node'));
+  // console.log(`最新目录：${process.cwd()}`)
+}
+
+fn();
--- Node/demo/push.js
+++ Node/demo/push.js
@@ -1,24 +1,39 @@
 const util = require('util');
+const path = require('path');
+const chalk = require('chalk');
 const exec = util.promisify(require('child_process').exec);
+// const chdir = util.promisify(process.chdir);
+const { prompt } = require('enquirer');
+const resolve = p => path.resolve(__dirname, p);
+const step = str => console.log(chalk.green(str));
 const addCommand = 'git add .';
 const commitCommand = `git commit -m `;
 const pushCommand = 'git push origin master';
+const promptOptions = {
+  type: 'input',
+  name: 'commitMessage',
+  message: '请输入commit信息：'
+}
 
 
 async function fn() {
-  let [commitMessage] = process.argv.splice(2);
+  const enquirerRes = await prompt(promptOptions);
+  const commitMessage = enquirerRes.commitMessage;
   if (!commitMessage) {
     throw new Error('缺少commit信息');
   }
-  console.log(`commitMessage: ${commitMessage}`);
+  step(`\n commitMessage: ${commitMessage}`);
+  // 修改执行目录
+  process.chdir(resolve('../../../frontend-demos'));
+  step(`\n 修改目录成功，当前执行目录：${process.cwd()}`)
   let res;
-  
+
   // add
   res = await exec(addCommand);
   if (res.err) {
     throw new Error('add err');
   } else {
-    console.log('add succ');
+    step('\n add succ');
   }
 
   // commit
@@ -26,7 +41,7 @@ async function fn() {
   if (res.err) {
     throw new Error('commit err');
   } else {
-    console.log('commit succ');
+    step('\n commit succ');
   }
 
   // push 
@@ -34,7 +49,7 @@ async function fn() {
   if (res.err) {
     throw new Error('push err');
   } else {
-    console.log('push finished');
+    step('\n push success');
   }
 }
 
