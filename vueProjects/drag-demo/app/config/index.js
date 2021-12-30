3aa9781326e0b687164fc5931876af00e9674711
yongqing_luo <953342912@qq.com>
2021-12-30 19:00:17 +0800

    :rocket: organize files

--- vueProjects/drag-demo/app/config/index.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  port: 8000,
-  mongodb: {
-    // MONDB_URL: 'mongodb://127.0.0.1:27017/user' // 本地
-    MONDB_URL: 'mongodb+srv://youngqluol:lyq7285600@cluster0.49eox.mongodb.net/user?retryWrites=true&w=majority' // 云端
-  }
-};
--- vueProjects/drag-demo/src/utils/registerComp.js
+++
@@ -1,7 +0,0 @@
-import Vue from 'vue';
-// import Text from '../components/Text.vue';
-import compListData from '../config/compListData'; // 组件数据
-
-compListData.forEach(item => {
-  Vue.component(item.component, require(`../components/${item.name}`).default);
-});
